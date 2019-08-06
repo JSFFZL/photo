c555a7ddefb47feb5961540351c926d7d55a6d1c
mqf <452926729@qq.com>
2019-08-07 00:34:01 +0800

    第二屏

---
+++ js/animation.js
@@ -0,0 +1,10 @@
+
+var logo = document.querySelector(".logo");
+
+// bounceInDown
+
+// setTimeout(()=>{
+//     // logo.classList.add("infinite");
+//     logo.classList.add("flash");
+//     logo.classList.remove("bounceInDown");
+// },9000)
--- js/indexSwiper.js
+++ js/indexSwiper.js
@@ -1,11 +1,41 @@
 
-var wrapper = new Swiper(".banner",{
-    direction:"vertical",
-    pagination:{
-        el:".swiper-pagination"
+var item1 = document.querySelector(".item1");
+var logo = document.querySelector(".logo");
+var logoImg = document.querySelector(".logo img");
+var lis = document.querySelectorAll(".item1 ul li");
+// var
+
+// (function(){
+//     logo.classList.add("bounceInDown");
+//     lis.forEach(function(item){
+//         item.classList.add("fadeIn");
+//     });
+//     logoImg.classList.add("flash");
+// })()
+
+
+
+var wrapper = new Swiper(".banner", {
+    direction: "vertical",
+    initialSlide: 2,
+    pagination: {
+        el: ".swiper-pagination"
     },
-    on:{
-        "slideChange":function(){
+    on: {
+        init: function () {
+            swiperAnimateCache(this);
+            swiperAnimate(this);
+        },
+        slideChangeTransitionEnd: function () {
+            swiperAnimate(this);
+            // this.slides.eq(this.activeIndex).find('.ani').removeClass('ani'); //动画只展现一次，去除ani类名
         }
+    }
+})
+
+var banner = new Swiper(".ad", {
+    loop: true,
+    autoplay: {
+        disableOnInteraction: false,
     }
 })
